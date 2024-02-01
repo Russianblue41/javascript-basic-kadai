6238042ed3053c0f55aeeac9439572d934052643
Russianblue41 <158019791+Russianblue41@users.noreply.github.com>
2024-02-01 20:38:19 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const date = () => {
+  const today = new Date();
+  console.log(today.getFullYear() + '年'); 
+  console.log(today.getMonth() + 1 + '月'); 
+  console.log(today.getDate() + '日');
+}
+
+date();
